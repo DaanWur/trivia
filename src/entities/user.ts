98971ca31c62b8776328ae68f229d072dbdcec16
Daniel Wurms <50828198+DaanWur@users.noreply.github.com>
2025-09-26 14:24:13 +0300

    Add Player and User entities with constructors and JSON serialization

---
+++ src/entities/user.ts
@@ -0,0 +1,16 @@
+import type { ID } from "./types.js";
+import { v4 as uuidv4 } from "uuid";
+
+export class User {
+  id: ID;
+  createdAt: string;
+
+  constructor() {
+    this.id = uuidv4();
+    this.createdAt = new Date().toISOString();
+  }
+
+  toJSON() {
+    return { id: this.id, createdAt: this.createdAt };
+  }
+}
