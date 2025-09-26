0f11e3cbaec90c288bf388b0404d9784fa8c246b
Daniel Wurms <50828198+DaanWur@users.noreply.github.com>
2025-09-26 14:19:13 +0300

    Add index file to export entity modules

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+export * from './types.js';
+export * from './user.js';
+export * from './player.js';
+export * from './category.js';
+export * from './question.js';
+export * from './round.js';
+export * from './match.js';
