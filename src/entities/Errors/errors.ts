8357309d5a4fc3c1c47b9a5382b6546176f3eb72
Daniel Wurms <50828198+DaanWur@users.noreply.github.com>
2025-09-26 14:19:22 +0300

    Add custom error classes for entity management

---
+++ src/entities/Errors/errors.ts
@@ -0,0 +1,27 @@
+export class EntityError extends Error {
+  constructor(message?: string) {
+    super(message);
+    this.name = "EntityError";
+  }
+}
+
+export class NotFoundError extends EntityError {
+  constructor(message?: string) {
+    super(message ?? "Not found");
+    this.name = "NotFoundError";
+  }
+}
+
+export class DuplicateError extends EntityError {
+  constructor(message?: string) {
+    super(message ?? "Duplicate entity");
+    this.name = "DuplicateError";
+  }
+}
+
+export class InvalidOperationError extends EntityError {
+  constructor(message?: string) {
+    super(message ?? "Invalid operation");
+    this.name = "InvalidOperationError";
+  }
+}
